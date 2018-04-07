5b0bd6928039c5b7974e89f37a30c3c989f41b45
dmokhtar <dmokhar@gmail.com>
2018-04-07 12:01:27 -0600

    adding skeleton for validation

---
+++ 3esi_BusinessLayer/Rules/Abstraction/IValidateRecord.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Esi_BusinessLayer.Abstraction;
+
+namespace Esi_BusinessLayer.Validate.Abstraction
+{
+    public interface IValidateRecord : IDisposable
+    {
+        List<IRecord> Records { get; set; }
+    }
+}
---
+++ 3esi_BusinessLayer/Rules/ValidateRecord.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Esi_BusinessLayer.Abstraction;
+using Esi_BusinessLayer.Validate.Abstraction;
+
+namespace Esi_BusinessLayer.Rules
+{
+    public class ValidateRecord : IValidateRecord
+    {
+
+        public void ValidateBusinessRules()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            Records?.Clear();
+        }
+
+        public List<IRecord> Records { get; set; }
+    }
+}
