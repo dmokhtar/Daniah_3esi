4c706baf5e1b3c26023126b9912cb7a4af723b8a
daniah <dmokhtar@gmail.com>
2018-04-08 09:05:45 -0600

    remove area methods not needed

--- 3esi_BusinessLayer/Rules/GroupDetails.cs
+++ 3esi_BusinessLayer/Rules/GroupDetails.cs
@@ -14,14 +14,6 @@ namespace Esi_BusinessLayer.Rules
             WellDetailsList = new List<WellDetails>();
         }
         public List<WellDetails> WellDetailsList { get; set; }
-
-        double Area { get; set; }
-
-        public double CalculateArea()
-        {
-            double radius = Radius;
-            double pi = Math.PI;
-            return Area = pi * (radius * radius);
-        }
+                
     }
 }
